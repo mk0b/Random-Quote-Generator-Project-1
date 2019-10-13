e04e9e9ad88ea1f2211160b8390a9959c6f0c71b
Megan Katherine O'Brien <megankob@gmail.com>
2019-10-13 15:16:34 -0400

    Update Ready for Review
    Finished last edits and cleanup. Ready for review!

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Random-Quote-Generator-Project-1
  Megan Katherine O'Brien's Random Quote Generator Project 1 as a TeamTreeHouse Full Stack JavaScript Talent Path student.
+
+Grading note: I am trying for exceeds expectations but ok with meets expectations.
+
+This program is a random quote generator. The quotes will change every 12 seconds and the user can change the quote by clicking the button. The background also randomly generates when the quote is changed or when the button is clicked to change the quote.
--- js/script.js
+++ js/script.js
@@ -3,31 +3,14 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
-//TODO: Make Pretty and customize my styling.
-//TODO: Make it timed to change the quote.
-//TODO: Delete the project instructions and add in my own notes.
-//TODO: Finish the README.md
-//TODO: ? it bothers me that the same quote (and now color) often comes up twice when clicking the show next quote button. See if I can get it to loop.
-//TODO: Figure out how to make it more responsive?
-
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
 /***
- * Quotes array with the following properties and their level of requirement.
+ * Created a Quotes array with the following properties and their level of requirement.
  * Required in all: quote, source
  * Required in at least 1: citation, year
  * Ones I'm adding myself: source title, quote type
  * Need at least 5 quotes. 
  */
+
 const quotes = [
   {
     quote: 'Build what you want to see in the world.',
@@ -83,41 +66,24 @@ const quotes = [
 
 console.log(quotes);
 
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
 
-//set randomNumber variable with the random number equation. 
-//Should use Math.ceil because we want to possibility of 0.
-//Almost made this too complicated but I just needed to store the random number into another variable and return that.
+/**I was using the wrong equation and had to ask for help in the end I received a
+ * hint and switched to using math.floor instead of math.ceil and subtracting by 1
+ * because an array.length starts at 1 and not 0.
+ * The below creates a random number and returns it.
+ */
+
 function getRandomQuote () {
   let randomNumber = Math.floor(Math.random() * (quotes.length - 1));
   let randomQuote = quotes[randomNumber];
   return randomQuote;
 }
 
-//testing TODO: remove this when done.
-console.log(getRandomQuote());
-
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/ 
-
 /*** 
  * The function below sets a random background color, decides which html to set dependant on if year or citation or both is unknown,
  * and returns the innerHTML of the html chosen. 
  */
+
 function printQuote () {
   let randomQuote = getRandomQuote();
   let html = '';
@@ -142,16 +108,11 @@ function printQuote () {
         //special html to add citation to show
         html = '<p class="quote">' + randomQuote.quote + '</p> <p class="source">' + randomQuote.source + '<span class="source-title">' + randomQuote.source_title +'</span><span class="citation">' + randomQuote.citation +'</span><span class="quote-type">' + randomQuote.quote_type +'</span></p>';
           }
-  //testing output trying to figure out why sometimes I get an error.
-  console.log(divQuoteBox.innerHTML = html);
   return  divQuoteBox.innerHTML = html;
 }
 
 /***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
+  Right below was existing code.
 ***/
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
@@ -159,5 +120,9 @@ document.getElementById('loadQuote').addEventListener("click", printQuote, false
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
 
-//Lauren recommended going with setInterval instead of window.setTimeout()
-setInterval('printQuote()', 10000);
+/*** I was originally trying to use a for loop with window.setTimeout() but then a
+ * fellow student recommended I use setInterval.
+ * The below changes the quote every 12 seconds. 
+ */
+
+setInterval('printQuote()', 12000);
